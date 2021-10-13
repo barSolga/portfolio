55f2d3347182741ec8e51abe2743bd466367a5a8
BarSolga-219165 <bartosz.solga@gmail.com>
2021-10-13 20:32:32 +0200

    finished projects section

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,9 +8,16 @@ const navToggle = document.querySelector('.nav__toggle');
 const toggleIcon = document.querySelector('.toggleIcon');
 const themeButton = document.querySelector('.switch');
 const websiteLogo = document.querySelector('.nav__logo');
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
+const projectBox = document.querySelector('.projects__elem');
 
+
+// set default website theme
 localStorage.setItem('theme', 'theme-light')
+
+// Get page url
+const url = window.location.toString();
+const siteURL = url.substring(0, url.lastIndexOf("/"));
 
 // toggle menu function 
 const showMenu = () => {
@@ -22,7 +29,7 @@ const showMenu = () => {
 // change theme
 const setTheme = (themeName) => {
     localStorage.setItem('theme', themeName);
-    body.className = themeName;
+    body.className = themeName; 
 }
 
 // event listeners
@@ -35,6 +42,40 @@ window.addEventListener('scroll', () => {
         nav.classList.remove('navScrolled');
     }
 })
+
+
+
+// Loading projects data on page
+fetch(`${siteURL}/assets/data/projects.json`)
+    .then(response => response.json())
+    .then(response => {
+
+        const wrapper = document.querySelector('.projects__box');
+        
+        // adding all projects form json file
+        for (let i = 0; i < response.project.length; i++) {
+            const project = projectBox.content.cloneNode(true);
+
+            project.querySelector('.projects__elem-image').setAttribute('src', response.project[i].image);
+            project.querySelector('.projects__elem-title').textContent = `${response.project[i].name}`;
+            project.querySelector('.projects__elem-link1').setAttribute('href', response.project[i].live);
+            project.querySelector('.projects__elem-link2').setAttribute('href', response.project[i].github);
+            
+            wrapper.appendChild(project)
+        }
+
+    })
+
+    // TODO write catch for api
+    // .catch(() => {
+        
+
+    //     warning.innerHTML = 'Wystąpił problem z załadowaniem produktów na naszej stronie!<br><span>Skontaktuj się z nami w celu otrzymania katalogu.</span>';
+    //     imgWarning.style.display = 'block';
+    // });
+    // 
+
+
 
 navLinks.forEach(item => {
     item.addEventListener('click', () => {
